d2f402de53ad71102f2b7f4b18e833760dbaa450
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-04-15 19:02:17 +0300

    adding spa settings

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-import { until } from '../node_modules/lit-html/directives/until.js';
-import page from '../node_modules/page/page.mjs';
+import { html, render } from '//unpkg.com/lit-html?module';
+import { until } from '//unpkg.com/lit-html/directives/until?module';
+import page from '//unpkg.com/page/page.mjs';
 
 export {
     html,
